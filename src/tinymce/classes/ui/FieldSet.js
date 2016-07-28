3f296e6db25a5d551f14ed2e3b51d72b937cc2b9
#云淡然 <cloudcome@163.com>
2016-07-28 16:26:07 +0800

    + blear.ui.rich-editor

--- src/tinymce/classes/ui/FieldSet.js
+++ src/tinymce/classes/ui/FieldSet.js
@@ -16,43 +16,42 @@
  * @extends tinymce.ui.Form
  */
 
-    var Form = require("./Form");
-    "use strict";
+"use strict";
+var Form = require("./Form");
 
-    return Form.extend({
-        Defaults: {
-            containerCls: 'fieldset',
-            layout: 'flex',
-            direction: 'column',
-            align: 'stretch',
-            flex: 1,
-            padding: "25 15 5 15",
-            labelGap: 30,
-            spacing: 10,
-            border: 1
-        },
+module.exports = Form.extend({
+    Defaults: {
+        containerCls: 'fieldset',
+        layout: 'flex',
+        direction: 'column',
+        align: 'stretch',
+        flex: 1,
+        padding: "25 15 5 15",
+        labelGap: 30,
+        spacing: 10,
+        border: 1
+    },
 
-        /**
-         * Renders the control as a HTML string.
-         *
-         * @method renderHtml
-         * @return {String} HTML representing the control.
-         */
-        renderHtml: function () {
-            var self = this, layout = self._layout, prefix = self.classPrefix;
+    /**
+     * Renders the control as a HTML string.
+     *
+     * @method renderHtml
+     * @return {String} HTML representing the control.
+     */
+    renderHtml: function () {
+        var self = this, layout = self._layout, prefix = self.classPrefix;
 
-            self.preRender();
-            layout.preRender(self);
+        self.preRender();
+        layout.preRender(self);
 
-            return (
-                '<fieldset id="' + self._id + '" class="' + self.classes + '" hidefocus="1" tabindex="-1">' +
-                (self.settings.title ? ('<legend id="' + self._id + '-title" class="' + prefix + 'fieldset-title">' +
-                self.settings.title + '</legend>') : '') +
-                '<div id="' + self._id + '-body" class="' + self.bodyClasses + '">' +
-                (self.settings.html || '') + layout.renderHtml(self) +
-                '</div>' +
-                '</fieldset>'
-            );
-        }
-    });
+        return (
+            '<fieldset id="' + self._id + '" class="' + self.classes + '" hidefocus="1" tabindex="-1">' +
+            (self.settings.title ? ('<legend id="' + self._id + '-title" class="' + prefix + 'fieldset-title">' +
+            self.settings.title + '</legend>') : '') +
+            '<div id="' + self._id + '-body" class="' + self.bodyClasses + '">' +
+            (self.settings.html || '') + layout.renderHtml(self) +
+            '</div>' +
+            '</fieldset>'
+        );
+    }
 });
