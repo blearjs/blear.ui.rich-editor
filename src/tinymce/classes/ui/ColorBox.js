35756f731db0590ce77922f6e04750236c904dd4
#云淡然 <cloudcome@163.com>
2016-07-28 16:24:03 +0800

    + blear.ui.rich-editor

--- src/tinymce/classes/ui/ColorBox.js
+++ src/tinymce/classes/ui/ColorBox.js
@@ -17,55 +17,54 @@
  * @extends tinymce.ui.ComboBox
  */
 
-    var ComboBox = require("./ComboBox");
-    "use strict";
+"use strict";
+var ComboBox = require("./ComboBox");
 
-    return ComboBox.extend({
-        /**
-         * Constructs a new control instance with the specified settings.
-         *
-         * @constructor
-         * @param {Object} settings Name/value object with settings.
-         */
-        init: function (settings) {
-            var self = this;
+module.exports = ComboBox.extend({
+    /**
+     * Constructs a new control instance with the specified settings.
+     *
+     * @constructor
+     * @param {Object} settings Name/value object with settings.
+     */
+    init: function (settings) {
+        var self = this;
 
-            settings.spellcheck = false;
+        settings.spellcheck = false;
 
-            if (settings.onaction) {
-                settings.icon = 'none';
-            }
-
-            self._super(settings);
-
-            self.classes.add('colorbox');
-            self.on('change keyup postrender', function () {
-                self.repaintColor(self.value());
-            });
-        },
-
-        repaintColor: function (value) {
-            var elm = this.getEl().getElementsByTagName('i')[0];
-
-            if (elm) {
-                try {
-                    elm.style.background = value;
-                } catch (ex) {
-                    // Ignore
-                }
-            }
-        },
-
-        bindStates: function () {
-            var self = this;
-
-            self.state.on('change:value', function (e) {
-                if (self._rendered) {
-                    self.repaintColor(e.value);
-                }
-            });
-
-            return self._super();
+        if (settings.onaction) {
+            settings.icon = 'none';
         }
-    });
+
+        self._super(settings);
+
+        self.classes.add('colorbox');
+        self.on('change keyup postrender', function () {
+            self.repaintColor(self.value());
+        });
+    },
+
+    repaintColor: function (value) {
+        var elm = this.getEl().getElementsByTagName('i')[0];
+
+        if (elm) {
+            try {
+                elm.style.background = value;
+            } catch (ex) {
+                // Ignore
+            }
+        }
+    },
+
+    bindStates: function () {
+        var self = this;
+
+        self.state.on('change:value', function (e) {
+            if (self._rendered) {
+                self.repaintColor(e.value);
+            }
+        });
+
+        return self._super();
+    }
 });
