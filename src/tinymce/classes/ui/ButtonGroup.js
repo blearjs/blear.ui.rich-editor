d0cbb270066c72496e3071902543954d0c5ea4ca
#云淡然 <cloudcome@163.com>
2016-07-28 16:23:35 +0800

    + blear.ui.rich-editor

--- src/tinymce/classes/ui/ButtonGroup.js
+++ src/tinymce/classes/ui/ButtonGroup.js
@@ -27,35 +27,35 @@
  * @extends tinymce.ui.Container
  */
 
-    var Container = require("./Container");
-    "use strict";
+"use strict";
 
-    return Container.extend({
-        Defaults: {
-            defaultType: 'button',
-            role: 'group'
-        },
+var Container = require("./Container");
 
-        /**
-         * Renders the control as a HTML string.
-         *
-         * @method renderHtml
-         * @return {String} HTML representing the control.
-         */
-        renderHtml: function () {
-            var self = this, layout = self._layout;
+module.exports = Container.extend({
+    Defaults: {
+        defaultType: 'button',
+        role: 'group'
+    },
 
-            self.classes.add('btn-group');
-            self.preRender();
-            layout.preRender(self);
+    /**
+     * Renders the control as a HTML string.
+     *
+     * @method renderHtml
+     * @return {String} HTML representing the control.
+     */
+    renderHtml: function () {
+        var self = this, layout = self._layout;
 
-            return (
-                '<div id="' + self._id + '" class="' + self.classes + '">' +
-                '<div id="' + self._id + '-body">' +
-                (self.settings.html || '') + layout.renderHtml(self) +
-                '</div>' +
-                '</div>'
-            );
-        }
-    });
+        self.classes.add('btn-group');
+        self.preRender();
+        layout.preRender(self);
+
+        return (
+            '<div id="' + self._id + '" class="' + self.classes + '">' +
+            '<div id="' + self._id + '-body">' +
+            (self.settings.html || '') + layout.renderHtml(self) +
+            '</div>' +
+            '</div>'
+        );
+    }
 });
--- src/tinymce/classes/ui/ClassList.js
+++ src/tinymce/classes/ui/ClassList.js
@@ -15,134 +15,133 @@
  * @class tinymce.ui.ClassList
  */
 
-    var Tools = require("../util/Tools");
-    "use strict";
+"use strict";
+var Tools = require("../util/Tools");
 
-    function noop() {
-    }
+function noop() {
+}
+
+/**
+ * Constructs a new class list the specified onchange
+ * callback will be executed when the class list gets modifed.
+ *
+ * @constructor ClassList
+ * @param {function} onchange Onchange callback to be executed.
+ */
+function ClassList(onchange) {
+    this.cls = [];
+    this.cls._map = {};
+    this.onchange = onchange || noop;
+    this.prefix = '';
+}
+
+Tools.extend(ClassList.prototype, {
+    /**
+     * Adds a new class to the class list.
+     *
+     * @method add
+     * @param {String} cls Class to be added.
+     * @return {tinymce.ui.ClassList} Current class list instance.
+     */
+    add: function (cls) {
+        if (cls && !this.contains(cls)) {
+            this.cls._map[cls] = true;
+            this.cls.push(cls);
+            this._change();
+        }
+
+        return this;
+    },
 
     /**
-     * Constructs a new class list the specified onchange
-     * callback will be executed when the class list gets modifed.
+     * Removes the specified class from the class list.
      *
-     * @constructor ClassList
-     * @param {function} onchange Onchange callback to be executed.
+     * @method remove
+     * @param {String} cls Class to be removed.
+     * @return {tinymce.ui.ClassList} Current class list instance.
      */
-    function ClassList(onchange) {
-        this.cls = [];
-        this.cls._map = {};
-        this.onchange = onchange || noop;
-        this.prefix = '';
+    remove: function (cls) {
+        if (this.contains(cls)) {
+            for (var i = 0; i < this.cls.length; i++) {
+                if (this.cls[i] === cls) {
+                    break;
+                }
+            }
+
+            this.cls.splice(i, 1);
+            delete this.cls._map[cls];
+            this._change();
+        }
+
+        return this;
+    },
+
+    /**
+     * Toggles a class in the class list.
+     *
+     * @method toggle
+     * @param {String} cls Class to be added/removed.
+     * @param {Boolean} state Optional state if it should be added/removed.
+     * @return {tinymce.ui.ClassList} Current class list instance.
+     */
+    toggle: function (cls, state) {
+        var curState = this.contains(cls);
+
+        if (curState !== state) {
+            if (curState) {
+                this.remove(cls);
+            } else {
+                this.add(cls);
+            }
+
+            this._change();
+        }
+
+        return this;
+    },
+
+    /**
+     * Returns true if the class list has the specified class.
+     *
+     * @method contains
+     * @param {String} cls Class to look for.
+     * @return {Boolean} true/false if the class exists or not.
+     */
+    contains: function (cls) {
+        return !!this.cls._map[cls];
+    },
+
+    /**
+     * Returns a space separated list of classes.
+     *
+     * @method toString
+     * @return {String} Space separated list of classes.
+     */
+
+    _change: function () {
+        delete this.clsValue;
+        this.onchange.call(this);
+    }
+});
+
+// IE 8 compatibility
+ClassList.prototype.toString = function () {
+    var value;
+
+    if (this.clsValue) {
+        return this.clsValue;
     }
 
-    Tools.extend(ClassList.prototype, {
-        /**
-         * Adds a new class to the class list.
-         *
-         * @method add
-         * @param {String} cls Class to be added.
-         * @return {tinymce.ui.ClassList} Current class list instance.
-         */
-        add: function (cls) {
-            if (cls && !this.contains(cls)) {
-                this.cls._map[cls] = true;
-                this.cls.push(cls);
-                this._change();
-            }
-
-            return this;
-        },
-
-        /**
-         * Removes the specified class from the class list.
-         *
-         * @method remove
-         * @param {String} cls Class to be removed.
-         * @return {tinymce.ui.ClassList} Current class list instance.
-         */
-        remove: function (cls) {
-            if (this.contains(cls)) {
-                for (var i = 0; i < this.cls.length; i++) {
-                    if (this.cls[i] === cls) {
-                        break;
-                    }
-                }
-
-                this.cls.splice(i, 1);
-                delete this.cls._map[cls];
-                this._change();
-            }
-
-            return this;
-        },
-
-        /**
-         * Toggles a class in the class list.
-         *
-         * @method toggle
-         * @param {String} cls Class to be added/removed.
-         * @param {Boolean} state Optional state if it should be added/removed.
-         * @return {tinymce.ui.ClassList} Current class list instance.
-         */
-        toggle: function (cls, state) {
-            var curState = this.contains(cls);
-
-            if (curState !== state) {
-                if (curState) {
-                    this.remove(cls);
-                } else {
-                    this.add(cls);
-                }
-
-                this._change();
-            }
-
-            return this;
-        },
-
-        /**
-         * Returns true if the class list has the specified class.
-         *
-         * @method contains
-         * @param {String} cls Class to look for.
-         * @return {Boolean} true/false if the class exists or not.
-         */
-        contains: function (cls) {
-            return !!this.cls._map[cls];
-        },
-
-        /**
-         * Returns a space separated list of classes.
-         *
-         * @method toString
-         * @return {String} Space separated list of classes.
-         */
-
-        _change: function () {
-            delete this.clsValue;
-            this.onchange.call(this);
-        }
-    });
-
-    // IE 8 compatibility
-    ClassList.prototype.toString = function () {
-        var value;
-
-        if (this.clsValue) {
-            return this.clsValue;
+    value = '';
+    for (var i = 0; i < this.cls.length; i++) {
+        if (i > 0) {
+            value += ' ';
         }
 
-        value = '';
-        for (var i = 0; i < this.cls.length; i++) {
-            if (i > 0) {
-                value += ' ';
-            }
+        value += this.prefix + this.cls[i];
+    }
 
-            value += this.prefix + this.cls[i];
-        }
+    return value;
+};
 
-        return value;
-    };
-
-    return ClassList;
-});
+module.exports = ClassList;
