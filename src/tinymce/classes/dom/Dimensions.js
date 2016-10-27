2696a9fa443a9edb84f29122f701a812a5211a4a
#云淡然 <cloudcome@163.com>
2016-10-27 17:47:01 +0800

    ~ up 4.4.3 > classes/dom

--- src/tinymce/classes/dom/Dimensions.js
+++ src/tinymce/classes/dom/Dimensions.js
@@ -16,47 +16,47 @@
  * @class tinymce.dom.Dimensions
  */
 
-    var Arr = require("../util/Arr");
-    var NodeType = require("../dom/NodeType");
-    var ClientRect = require("../geom/ClientRect");
+var Arr = require("../util/Arr");
+var NodeType = require("../dom/NodeType");
+var ClientRect = require("../geom/ClientRect");
 
-    function getClientRects(node) {
-        function toArrayWithNode(clientRects) {
-            return Arr.map(clientRects, function (clientRect) {
-                clientRect = ClientRect.clone(clientRect);
-                clientRect.node = node;
+function getClientRects(node) {
+    function toArrayWithNode(clientRects) {
+        return Arr.map(clientRects, function (clientRect) {
+            clientRect = ClientRect.clone(clientRect);
+            clientRect.node = node;
 
-                return clientRect;
-            });
-        }
-
-        if (Arr.isArray(node)) {
-            return Arr.reduce(node, function (result, node) {
-                return result.concat(getClientRects(node));
-            }, []);
-        }
-
-        if (NodeType.isElement(node)) {
-            return toArrayWithNode(node.getClientRects());
-        }
-
-        if (NodeType.isText(node)) {
-            var rng = node.ownerDocument.createRange();
-
-            rng.setStart(node, 0);
-            rng.setEnd(node, node.data.length);
-
-            return toArrayWithNode(rng.getClientRects());
-        }
+            return clientRect;
+        });
     }
 
-    module.exports = {
-        /**
-         * Returns the client rects for a specific node.
-         *
-         * @method getClientRects
-         * @param {Array/DOMNode} node Node or array of nodes to get client rects on.
-         * @param {Array} Array of client rects with a extra node property.
-         */
-        getClientRects: getClientRects
-    };
+    if (Arr.isArray(node)) {
+        return Arr.reduce(node, function (result, node) {
+            return result.concat(getClientRects(node));
+        }, []);
+    }
+
+    if (NodeType.isElement(node)) {
+        return toArrayWithNode(node.getClientRects());
+    }
+
+    if (NodeType.isText(node)) {
+        var rng = node.ownerDocument.createRange();
+
+        rng.setStart(node, 0);
+        rng.setEnd(node, node.data.length);
+
+        return toArrayWithNode(rng.getClientRects());
+    }
+}
+
+module.exports = {
+    /**
+     * Returns the client rects for a specific node.
+     *
+     * @method getClientRects
+     * @param {Array/DOMNode} node Node or array of nodes to get client rects on.
+     * @param {Array} Array of client rects with a extra node property.
+     */
+    getClientRects: getClientRects
+};
