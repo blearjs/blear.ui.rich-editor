497d146a075897cbf5f1bfe03168812c9af8fcdb
#云淡然 <cloudcome@163.com>
2016-11-30 17:43:11 +0800

    ~ 设置元素尺寸统一用 style

--- src/tinymce/classes/dom/ControlSelection.js
+++ src/tinymce/classes/dom/ControlSelection.js
@@ -208,12 +208,7 @@ module.exports = function (selection, editor) {
 
         function setSizeProp(name, value) {
             if (value) {
-                // Resize by using style or attribute
-                if (selectedElm.style[name] || !editor.schema.isValid(selectedElm.nodeName.toLowerCase(), name)) {
-                    dom.setStyle(selectedElm, name, value);
-                } else {
-                    dom.setAttrib(selectedElm, name, value);
-                }
+                dom.setStyle(selectedElm, name, value);
             }
         }
 
