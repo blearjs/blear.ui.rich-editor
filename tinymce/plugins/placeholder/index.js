7a2fe2efe7319c73b7ad84c9c87bee8c46ab5d60
#云淡然 <cloudcome@163.com>
2018-09-07 15:27:18 +0800

    ~ 更精确的 placeholder

--- tinymce/plugins/placeholder/index.js
+++ tinymce/plugins/placeholder/index.js
@@ -1,5 +1,5 @@
 /**
- * 文件描述
+ * 占位字符插件
  * @author ydr.me
  * @create 2018-09-06 17:54
  * @update 2018-09-06 17:54
@@ -7,6 +7,8 @@
 
 
 'use strict';
+
+var selector = require('blear.core.selector');
 
 tinymce.PluginManager.add('placeholder', function (editor) {
     editor.on('init', function () {
@@ -27,11 +29,38 @@ tinymce.PluginManager.add('placeholder', function (editor) {
         );
 
         var display = null;
+        var isEmpty = function () {
+            var bodyEl = editor.getBody();
+            var els = selector.children(bodyEl);
+
+            // 如果有多个段落
+            if (els.length > 1) {
+                return false;
+            }
+
+            var pEl = els[0];
+
+            if (!pEl) {
+                return true;
+            }
+
+            var text = pEl.innerText.trim();
+
+            // 如果有文本内容
+            if (text) {
+                return false;
+            }
+
+            // 再判断当前段落有没有非 BR
+            els = selector.children(pEl);
+            els = selector.filter(els, function (el) {
+                return el.tagName.toLowerCase() !== 'br';
+            });
+
+            return els.length === 0;
+        };
         var check = function () {
-            var text = editor.getContent({
-                format: 'text'
-            }).trim();
-            var _display = text === '';
+            var _display = isEmpty();
 
             if (_display === display) {
                 return;
